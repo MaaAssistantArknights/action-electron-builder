e72c6b7e07feeed53e3c25842616cbb316828ef1
bakashigure <bakashigure@hotmail.com>
2023-01-17 09:58:50 +0000

    fix script

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ const runAction = () => {
 	for (let i = 0; i < maxAttempts; i += 1) {
 		try {
 			run(
-				`${usepnpm ? "pnpm run" : "npx --no-install"} ${cmd} --${platform} ${
+				`${usepnpm ? "pnpm" : "npx --no-install"} ${cmd} --${platform} ${
 					release ? "--publish always" : ""
 				} ${args}`,
 				appRoot,
